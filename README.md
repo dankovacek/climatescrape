701acf22d75142c2419573b336b422b900bc2209
Danbot <dankovacek@gmail.com>
2017-12-07 09:15:49 -0800

    concatenate downloaded files and save to DATA_DIR

--- README.md
+++ README.md
@@ -15,3 +15,30 @@ but it can be worked around without too much trouble.
   3.  If virtualenv not activated: `>>source env/bin/activate` (from main folder)
   4.  `>>pip install requirements.txt`
   5.  `>>python main.py`
+  6.  Get coordinates for location(s) (in Canada) you want to check for the nearest
+      stations.
+  7.  Enter the coordinates in lat/lon (or just as a tuple in UTM northing/easting):
+
+  ```
+  # input target location decimal degrees [lat, lon]
+  squamish_utm_loc = utm.from_latlon(50.336, -122.802161)
+  pemberton_utm_loc = utm.from_latlon(49.796, -123.203)
+  ```
+
+  8.  Set a search radius (in m) to bound your search from the target location:
+  `search_radius = 10000`
+  9.  Set a timeframe (1 = hourly, 2 = daily, 3 = monthly), e.g.:  
+  `t_frame = 2`  
+
+### Execution
+
+The example code has a default random lat/lon location near Squamish, BC.
+
+Once you have addressed all of the input parameters in `Setup`:
+  * `>>python main.py`
+
+Watch the `data` folder for csv files being written.  
+
+Enjoy!
+
+If you get use out of this, let me know!
--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import os
 import sys
 import time
 import utm
-import wget
 
 from helper_functions import load_data
 
@@ -19,8 +18,8 @@ print('dirname = ', DATA_DIR)
 stn_df = load_data('Station_Inventory_EN.csv', DATA_DIR)
 
 # input target location decimal degrees [lat, lon]
-squamish_utm_loc = utm.from_latlon(50.336, -122.802161)
-pemberton_utm_loc = utm.from_latlon(49.796, -123.203)
+pemberton_utm_loc = utm.from_latlon(50.336, -122.802161)
+squamish_utm_loc = utm.from_latlon(49.796, -123.203)
 
 stn_df['distance_to_squamish'] = np.sqrt((stn_df['utm_E'] - squamish_utm_loc[0])**2 +
                                          (stn_df['utm_N'] - squamish_utm_loc[1])**2)
@@ -29,29 +28,40 @@ stn_df['distance_to_squamish'] = np.sqrt((stn_df['utm_E'] - squamish_utm_loc[0])
 stn_df['distance_to_pemberton'] = np.sqrt((stn_df['utm_E'] - pemberton_utm_loc[0])**2 +
                                           (stn_df['utm_N'] - pemberton_utm_loc[1])**2)
 
+# enter the distance from the target to search for stations
+search_radius = 10000
+
 # pull the station IDs for all stations within 10km of stations
-pemberton_stns = stn_df[stn_df['distance_to_pemberton'] < 10000]
-pemberton_stns.dropna(axis=0, how='any', subset=[
-                      'MLY First Year', 'MLY Last Year'], inplace=True)
-squamish_stns = stn_df[stn_df['distance_to_squamish'] < 10000]
-squamish_stns.dropna(axis=0, how='any', subset=[
-                     'MLY First Year', 'MLY Last Year'], inplace=True)
-# print(pemberton_stns.head())
+pemberton_stns = stn_df[stn_df['distance_to_pemberton'] < search_radius]
+pemberton_stns = pemberton_stns.dropna(axis=0, how='any', subset=[
+    'MLY First Year', 'MLY Last Year'])
+squamish_stns = stn_df[stn_df['distance_to_squamish'] < search_radius]
+squamish_stns = squamish_stns.dropna(axis=0, how='any', subset=[
+    'MLY First Year', 'MLY Last Year'])
 
 t_frame = 2  # 2 corresponds to daily, 1 to hourly, 3 to monthly
 # for hourly, need to add '&Day={}' back in following '&month={}'
-year = 1990
-month = [e for e in range(1, 13)]
-station_id = 1
 
-for index, row in pemberton_stns.iterrows():
-    years = [e for e in range(
-        int(row['MLY First Year']), int(row['MLY Last Year']) + 1)]
+for index, row in squamish_stns.iterrows():
+    rec_start = int(row['MLY First Year'])
+    rec_end = int(row['MLY Last Year'])
+    years = [e for e in range(rec_start, rec_end + 1)]
+    frames = []
+    all_data = pd.DataFrame()
+
     for year in years:
         ec_base_url = 'http://climate.weather.gc.ca/climate_data/bulk_data_e.html?'
         ec_url = ec_base_url + 'format=csv&stationID={}&Year={}&Month={}&Day=14&timeframe={}&submit=Download+Data'.format(
             row['Station ID'], year, 1, t_frame)
-        print(ec_url)
-        filename = wget.download(ec_url)
 
-        print(filename)
+        df = pd.read_csv(ec_url, header=23, parse_dates=['Date/Time'])
+
+        frames += [df]
+
+    all_data = pd.concat(frames)
+    stn_name = str(stn_df[stn_df['Station ID'] ==
+                          row['Station ID']].Name.item())
+    new_file_name = os.path.join(DATA_DIR, stn_name + '_' +
+                                 str(row['Station ID']).strip() + '_ID' +
+                                 str(rec_start) + '_to_' + str(rec_end) + '.csv')
+    all_data.to_csv(new_file_name)
